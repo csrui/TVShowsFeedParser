1769470546dd7bf6be8676571aa2577d30690c62
Rui Cruz <mail@ruicruz.com>
2013-03-20 11:52:40 +0000

    refactored code into an object

--- tvshows.php
+++ tvshows.php
@@ -1,39 +1,18 @@
 <?php
 
 include 'tvshows.config.php';
+include 'libs/parser.php';
 
 try {
 
-	$doc = new DOMDocument();
-	$doc->preserveWhiteSpace = false;
-
-	$doc->load(sprintf(FEED_URL, $_GET['show']));
-	$xpath = new DOMXPath($doc);
+	$parser = new Parser(FEED_URL, $_GET['show']);
 
 	# Filter HD or SD quality
 
-	$nodes_to_remove = array();
+	if (!empty($_GET['filter'])) {
 
-	if ($_GET['sd'] == true) {
+		$parser->filter($_GET['filter']);
 
-		$selected_nodes = $xpath->query('//channel/item/title[(contains(., "720p"))]');
-
-	} else {
-
-		$selected_nodes = $xpath->query('//channel/item/title[not(contains(., "720p"))]');
-
-	}	
-	
-	foreach ($selected_nodes as $elem) {
-
-	    $nodes_to_remove[] = $elem->parentNode;
-
-	}
-
-	if (count($nodes_to_remove) > 0) {
-		foreach($nodes_to_remove as $node) {
-			$node->parentNode->removeChild($node);
-		}
 	}
 
 
@@ -41,31 +20,14 @@ try {
 
 	if (!empty($_GET['starts'])) {
 
-		$nodes_to_remove = array();
-		$starts = explode(':', $_GET['starts']);
-
-		foreach($xpath->query('//channel/item/title') as $title) {
-
-			preg_match('/(?:S|season)\W?(\d{1,2})(?:E|episode)\W?(\d{1,2})/', $title->nodeValue, $matches);
-
-			if ($starts[0] > $matches[1] || ($starts[0] == $matches[1] && $starts[1] > $matches[2])) {
-				$nodes_to_remove[] = $title->parentNode;
-			}
-
-		}
-
-		if (count($nodes_to_remove) > 0) {
-			foreach($nodes_to_remove as $node) {
-				$node->parentNode->removeChild($node);
-			}
-		}
+		$parser->resume($_GET['starts']);
 
 	}
 	
 	# Output result feed
 
 	header('Content-type: application/xml');
-	echo $doc->saveXML();
+	echo $parser->getFeed();
 	
 } catch (Exception $e) {
 
