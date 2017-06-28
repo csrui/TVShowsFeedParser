0546c056d5aaf8585dfaecfc1c3201ae06215e7f
PXgamer <owzie123@gmail.com>
2017-06-28 14:51:43 +0100

    PSR-2 formatting for the Parser class

--- src/Parser.php
+++ src/Parser.php
@@ -2,78 +2,83 @@
 
 namespace csrui\TVShowsFeedParser;
 
-class Parser {
+class Parser
+{
 
-	private $doc = null;
+    private $doc = null;
 
-	private $xpath = null;
+    private $xpath = null;
 
-	private $parsed = null;
+    private $parsed = null;
 
 
-	public function __construct($feed, $show) {
+    public function __construct($feed, $show)
+    {
 
-		$this->doc = new \DOMDocument();
-		$this->doc->preserveWhiteSpace = false;
+        $this->doc = new \DOMDocument();
+        $this->doc->preserveWhiteSpace = false;
 
-		$this->doc->load(sprintf(Config::feedUrl(), $show));
-		$this->xpath = new \DOMXPath($this->doc);
+        $this->doc->load(sprintf(Config::feedUrl(), $show));
+        $this->xpath = new \DOMXPath($this->doc);
 
-	}
+    }
 
-	public function filter($string, $exclude = false) {
+    public function filter($string, $exclude = false)
+    {
 
-		$nodes_to_remove = array();
+        $nodes_to_remove = array();
 
-		if ($exclude === true) {
+        if ($exclude === true) {
 
-			$selected_nodes = $this->xpath->query(sprintf('//channel/item/title[(contains(., "%s"))]', $string));
+            $selected_nodes = $this->xpath->query(sprintf('//channel/item/title[(contains(., "%s"))]', $string));
 
-		} else {
+        } else {
 
-			$selected_nodes = $this->xpath->query(sprintf('//channel/item/title[not(contains(., "%s"))]', $string));
+            $selected_nodes = $this->xpath->query(sprintf('//channel/item/title[not(contains(., "%s"))]', $string));
 
-		}	
-		
-		foreach ($selected_nodes as $elem) {
+        }
 
-		    $nodes_to_remove[] = $elem->parentNode;
+        foreach ($selected_nodes as $elem) {
 
-		}
+            $nodes_to_remove[] = $elem->parentNode;
 
-		if (count($nodes_to_remove) > 0) {
-			foreach($nodes_to_remove as $node) {
-				$node->parentNode->removeChild($node);
-			}
-		}
+        }
 
-	}
+        if (count($nodes_to_remove) > 0) {
+            foreach ($nodes_to_remove as $node) {
+                $node->parentNode->removeChild($node);
+            }
+        }
 
-	public function resume($episode) {
+    }
 
-		$nodes_to_remove = array();
-		$starts = explode(':', $episode);
+    public function resume($episode)
+    {
 
-		foreach($this->xpath->query('//channel/item/title') as $title) {
+        $nodes_to_remove = array();
+        $starts = explode(':', $episode);
 
-			preg_match('/(?:S|season)\W?(\d{1,2})(?:E|episode)\W?(\d{1,2})/', $title->nodeValue, $matches);
+        foreach ($this->xpath->query('//channel/item/title') as $title) {
 
-			if ($starts[0] > $matches[1] || ($starts[0] == $matches[1] && $starts[1] > $matches[2])) {
-				$nodes_to_remove[] = $title->parentNode;
-			}
+            preg_match('/(?:S|season)\W?(\d{1,2})(?:E|episode)\W?(\d{1,2})/', $title->nodeValue, $matches);
 
-		}
+            if ($starts[0] > $matches[1] || ($starts[0] == $matches[1] && $starts[1] > $matches[2])) {
+                $nodes_to_remove[] = $title->parentNode;
+            }
 
-		if (count($nodes_to_remove) > 0) {
-			foreach($nodes_to_remove as $node) {
-				$node->parentNode->removeChild($node);
-			}
-		}
+        }
 
-	}
+        if (count($nodes_to_remove) > 0) {
+            foreach ($nodes_to_remove as $node) {
+                $node->parentNode->removeChild($node);
+            }
+        }
 
-	public function getFeed() {
-		return $this->doc->saveXML();
-	}
+    }
+
+    public function getFeed()
+    {
+        return $this->doc->saveXML();
+    }
 
 }
