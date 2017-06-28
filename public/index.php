213946f204adb00c063e69598d4810791ac80870
PXgamer <owzie123@gmail.com>
2017-06-28 14:54:28 +0100

    PSR-2 spacing changes

--- public/index.php
+++ public/index.php
@@ -6,32 +6,32 @@ include __DIR__ . '/../vendor/autoload.php';
 
 try {
 
-	$parser = new TVShowsFeedParser\Parser($_GET['show']);
+    $parser = new TVShowsFeedParser\Parser($_GET['show']);
 
-	# Filter HD or SD quality
+    # Filter HD or SD quality
 
-	if (!empty($_GET['filter'])) {
+    if (!empty($_GET['filter'])) {
 
-		$parser->filter($_GET['filter']);
+        $parser->filter($_GET['filter']);
 
-	}
+    }
 
 
-	# Filter from a specific episode and up
+    # Filter from a specific episode and up
 
-	if (!empty($_GET['starts'])) {
+    if (!empty($_GET['starts'])) {
 
-		$parser->resume($_GET['starts']);
+        $parser->resume($_GET['starts']);
 
-	}
+    }
 
-	# Output result feed
+    # Output result feed
 
-	header('Content-type: application/xml');
-	echo $parser->getFeed();
+    header('Content-type: application/xml');
+    echo $parser->getFeed();
 
 } catch (Exception $e) {
 
-	die($e->getMessage());
+    die($e->getMessage());
 
 }
